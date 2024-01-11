e6f223f99384dc5188e5e8cf673658d1c6b9c7ad
Gavin Davenport <gavinkdavenport@gmail.com>
2024-01-10 21:01:06 -0800

    Allow for multiple operations, update TODO
    change function so it will chain operations

--- README.md
+++ README.md
@@ -3,16 +3,16 @@ Calculator project for The Odin Project learning.
 
 TO DO:
 
-1.  Clean up display output styling, flex size to stay in screen limits
+1.  Allow for multiple numbers in a row to be seen as one number
 
-2.  Fix bugs and allow for multiple operators
+2.  Round answers so no overflow
 
-3.  Adjust function to work with multi-digit numbers
+3.  Adjust operator keys so no multiple operators in a row
 
-4.  Adjust operator keys so no multiple operators in a row
+4.  Adjust function to work with multi-digit numbers
 
-5.  Round answers so no overflow
+5.  Add decimal input functionality
 
-6.  Add decimal input functionality
+6.  See if dynamic text resizing is possible if input is very long
 
 7. (optional) keyboard support
--- calculator.js
+++ calculator.js
@@ -2,7 +2,7 @@ function operate(arr) {
     if (arr[1] === '+') {
         return parseInt(arr[0]) + parseInt(arr[2]);
     } else if (arr[1] === '–') {
-        return arr[0] -= arr[2];
+        return arr[0] - arr[2];
     } else if (arr[1] === '×') {
         return arr[0] * arr[2];
     } else if (arr[1] === '÷'){
@@ -48,9 +48,32 @@ clear.addEventListener("click", () => {
 
 let execute = document.querySelector('#execute');
 execute.addEventListener("click", () => {
-    displayValue.unshift(operate(displayValue));
-    for (let i = 0; i < 3; i++) {
-        displayValue.pop();
+    while (displayValue.length >= 3) {
+        displayValue.unshift(operate(displayValue));
+        displayValue.splice(1, 3);
     };
     populateDisplay();
 });
+
+
+/* auto text resize attempt
+
+const displayContainer = document.querySelector('.display');
+
+const isOverflown = ({element}) => element.scrollHeight > element.innerHeight;
+
+const resizeText = ({element, parent}) => {
+    let p = 20;
+    let overflow = false;
+    const maxSize = 40;
+
+    while (!overflow && p < maxSize) {
+        element.style.fontsize = `${p}px`;
+        overflow = isOverflown(parent);
+        if (!overflow) p++;
+    };
+
+    element.style.fontSize = `${p - 1}px`;
+};
+
+resizeText({display, displayContainer}); */
